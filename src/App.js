d76006bbd1bcd788d666bee023dac771d7a57aa2
rakshixh-me <rakshixh.socials@gmail.com>
2024-09-19 22:23:03 +0530

    added end text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ function App() {
       <ProfileCard />
       <SocialCards />
       <Link ArrayOfLinks={Links.mainLinks} />
+      <a
+        href="https://github.com/rakshixh"
+        target="_blank"
+        rel="noreferrer"
+        className="endText"
+        style={{ animation: "fadeInEffect 2s ease-in-out forwards" }}
+      >
+        <p className="endText">rakshixh</p>
+      </a>
     </div>
   );
 }
