b813a2b4d1962e94aae2cbdda042b37bf1159b0a
rakshixh-me <rakshixh.socials@gmail.com>
2025-01-10 11:41:22 +0530

    image blob  to base64

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,4 @@
 export const fetchImage = async () => {
-    // const devImageURL = 'https://raw.githubusercontent.com/rakshixh-me/self-hosted-server/main/assets/rakshixhDev.webp';
     const buildType = process.env.REACT_APP_BUILD;
     const imageName = process.env[`REACT_APP_${buildType.toUpperCase()}_IMAGE_NAME`];
     const imageURL = `https://raw.githubusercontent.com/${process.env.REACT_APP_GIT_USER_NAME}/${process.env.REACT_APP_GIT_REPO_NAME}/${process.env.REACT_APP_GIT_BRANCH_NAME}/assets/${imageName}`;
@@ -10,8 +9,14 @@ export const fetchImage = async () => {
       }
       // Converting to blob
       const blob = await response?.blob();
-      const objectURL = URL?.createObjectURL(blob);
-      return objectURL; // Return the image URL
+      // Convert blob to Base64
+      const base64Data = await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader?.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(blob);
+      });
+      return base64Data; // Return Base64 string
     } catch (error) {
       console.error('Error fetching image:', error);
       return null;
@@ -19,7 +24,6 @@ export const fetchImage = async () => {
   };
   
   export const fetchData = async () => {
-    // const jsonURL = 'https://raw.githubusercontent.com/rakshixh-me/self-hosted-server/main/json/dev.json';
     const buildType = process.env.REACT_APP_BUILD;
     const jsonName = process.env[`REACT_APP_${buildType.toUpperCase()}_JSON_NAME`];
     const jsonURL = `https://raw.githubusercontent.com/${process.env.REACT_APP_GIT_USER_NAME}/${process.env.REACT_APP_GIT_REPO_NAME}/${process.env.REACT_APP_GIT_BRANCH_NAME}/json/${jsonName}`;
